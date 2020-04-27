aa7739538fd2d5ad497c6e8a4ebb07e79fc16db4
孙菁 <sunjing@hoge.cn>
2020-04-27 17:41:08 +0800

    update store

---
+++ src/store/user.js
@@ -0,0 +1,36 @@
+import us from '../services/user'
+
+export default ({
+  state: {
+    isLogin: !!localStorage.getItem('token')
+  },
+  mutations: {
+    setLoginState (state, b) {
+      state.isLogin = b
+    }
+  },
+  actions: {
+    login ({ commit }, user) {
+      return us.login(user)
+        .then(res => {
+          const { code, token } = res.data
+          if (code) {
+            // 登录成功 保存登录状态 保存token
+            commit('setLoginState', true)
+            localStorage.setItem('token', token)
+          }
+          return code
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    logout ({ commit }) {
+      // 清除token 重置登录状态
+      localStorage.removeItem('token')
+      commit('setLoginState', false)
+    }
+  },
+  modules: {
+  }
+})
